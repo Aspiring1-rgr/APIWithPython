4bffa1565724cbeb5212639b758083b27bd94362
Richard <rohde122195@gmail.com>
2025-06-17 23:07:54 -0400

    Added API's and data search route.

--- lab/server.py
+++ lab/server.py
@@ -2,10 +2,69 @@
 This program is just practice constructing RESTful API's using Python and Flask.
 """
 # Import the Flask class from the flask module
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 # Create an instance of the Flask class, passing in the name of the current module
 app = Flask(__name__)
+
+# Hardcoded data, in lieu of an actual database
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    }
+]
 
 # Define a route for the root URL ("/")
 @app.route("/")
@@ -40,3 +99,43 @@ def index_explicit():
     Also note, because I keep doing it for some reason, your status codes do not go in curly brackets.
     """
     return resp 
+
+@app.route("/data")
+def get_data():
+    try:
+        # Check if data exists and has a length greater than 0
+        if data and len(data) > 0:
+            # Return a JSON response with a message indicating the length of the data
+            return {'message' : f'Data of length {len(data)} found'}
+        else:
+            # If data is empty, return a JSON response and error code 500
+            return {'message' : 'Data is empty'}, 500
+    except:
+        # If data is not defined, return a JSON response and error code 404
+        return {'message' : 'Data not found'}, 404
+
+"""
+For the next route, we will create a name_search method which will look for a 'q' argument in the request URL.
+We will need to import request from flask for that.
+"""
+@app.route('/name_search')
+def name_search():
+    # Get the argument 'q' from the query parameters of the request
+    query = request.args.get('q')
+
+    # Check if the parameter is missing
+    if query is None:
+        return {'message' : 'Query parameter is missing'}
+
+    # Check if the parameter is present but invalid
+    if query.strip() == "" or query.isdigit():
+        return {'message' : 'Invalid input'}
+
+    # Iterate through the 'data' list to look for the person whose first name matches the query
+    # We use the .lower method to make sure that the query is not case sensitive
+    for person in data:
+        if query.lower() in person['first_name'].lower():
+            return person, 200
+    
+    # if no match is found, return a JSON response indicating a 404
+    return {'message' : 'Person not found'}, 404
